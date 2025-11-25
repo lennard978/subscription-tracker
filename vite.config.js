8512d178fd12d134b5fb6aa5daa5cc509398351b
Lennard978 <shparavalo78nenad@gmail.com>
2025-11-25 21:36:45 +0100

    Changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/subscription-tracker/", // <--- MUST match your GitHub repo name
+  plugins: [react()],// <--- MUST match your GitHub repo name
 });
